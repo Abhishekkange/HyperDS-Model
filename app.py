0ec3e7db5fd35ea2279dbd4866139a5f4378c35b
Abhishek <Abhishekkange@gmail.com>
2024-04-26 22:54:50 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import torch
+from diffusers import DiffusionPipeline, DDIMScheduler
+from huggingface_hub import hf_hub_download
+base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+repo_name = "ByteDance/Hyper-SD"
+# Take 2-steps lora as an example
+ckpt_name = "Hyper-SDXL-2steps-lora.safetensors"
+# Load model.
+pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
+pipe.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
+pipe.fuse_lora()
+# Ensure ddim scheduler timestep spacing set as trailing !!!
+pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+# lower eta results in more detail
+print("model loaded")
+prompt="a boy wearing a red T shirt and having horizontal white strips on it."
+print("generating...")
+image=pipe(prompt=prompt, num_inference_steps=2, guidance_scale=0).images[0]
+print(type(image))
